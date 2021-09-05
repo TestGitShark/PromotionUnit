a813683e066e32bd4bd2a8eab0e17e14431829b0
TestGitShark <TestGitShark@gmail.com>
2021-09-05 12:42:09 +0200

    IsmultibuyPromotionApplicable + unit Test

--- PromotionUnit.Tests/UnitTest1.cs
+++ PromotionUnit.Tests/UnitTest1.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using Xunit;
-
+using PromotionUnit;
+using System.Collections.Generic;
 namespace PromotionUnit.Tests
 {
-    public class UnitTest1
+    public class ProgramTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData('A', 4, 'A', 3, 120, true)]
+        [InlineData('A', 3, 'A', 3, 120, true)]
+        [InlineData('B', 3, 'B', 2, 50, true)]
+        public void IsMultibuyPromotionApplicable_ReturnsTrue(char cartProductId, int cartProductCount,
+                                                            char promotionProductId, int promotionProductCount,
+                                                            double promotionProductPrice, bool promotionState)
         {
+            //Arrange
+            bool expectedResult = true;
+            CartItem cartItem = new CartItem { ProductId = cartProductId, Count = cartProductCount };
+            Dictionary<char, MultibuyPromotion> dictionaryMultibuyPromotion = new Dictionary<char, MultibuyPromotion>();
+            dictionaryMultibuyPromotion.Add(promotionProductId, new MultibuyPromotion { Id = promotionProductId, Count = promotionProductCount, Price = promotionProductPrice, ActiveState = promotionState });
+            // dictionaryMultibuyPromotion.Add('B', new MultibuyPromotion { Id = 'B', Count = 2, Price = 50, ActiveState = true });
 
+            //Act
+
+            bool actualResult = Program.IsMultibuyPromotionApplicable(cartItem, dictionaryMultibuyPromotion);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+
+        [Theory]
+        [InlineData('A', 2, 'A', 3, 120, true)]
+        [InlineData('A', 3, 'A', 3, 120, false)]
+        [InlineData('B', 3, 'A', 3, 120, true)]
+        public void IsMultibuyPromotionApplicable_ReturnsFalse(char cartProductId, int cartProductCount,
+                                                            char promotionProductId, int promotionProductCount,
+                                                            double promotionProductPrice, bool promotionState)
+        {
+            //Arrange
+            bool expectedResult = false;
+            CartItem cartItem = new CartItem { ProductId = cartProductId, Count = cartProductCount };
+            Dictionary<char, MultibuyPromotion> dictionaryMultibuyPromotion = new Dictionary<char, MultibuyPromotion>();
+            dictionaryMultibuyPromotion.Add(promotionProductId, new MultibuyPromotion { Id = promotionProductId, Count = promotionProductCount, Price = promotionProductPrice, ActiveState = promotionState });
+            // dictionaryMultibuyPromotion.Add('B', new MultibuyPromotion { Id = 'B', Count = 2, Price = 50, ActiveState = true });
+
+            //Act
+
+            bool actualResult = Program.IsMultibuyPromotionApplicable(cartItem, dictionaryMultibuyPromotion);
+
+            //Assert
+            Assert.Equal(expectedResult, actualResult);
         }
     }
 }
--- PromotionUnit/Program.cs
+++ PromotionUnit/Program.cs
@@ -1,19 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-
+/*
+ Promotion Unit applies currently active promotions on a given shopping cart
+ASSUMPTIONS
+1. Only one promotion per product
+2. On the shopping cart , one entry per product, and total count of the product
+ 
+ */
 namespace PromotionUnit
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                try
+                {
 
-            //Load demo data
-            Dictionary<char, int> dictionaryUnitPrice = DemoDataManager.LoadUnitPriceData();
-            List<CartItem> shoppingCart = DemoDataManager.LoadShoppingCartData();
-            Dictionary<char, MultibuyPromotion> dictionaryMultibuyPromotion = DemoDataManager.LoadMultibuyPromotions();
-            Dictionary<char, DuoComboPromotion> dictionaryDuoComboPromotion = DemoDataManager.LoadDuoComboPromotions();
+            
+
+                    //Load demo data
+                    Dictionary<char, int> dictionaryUnitPrice = DemoDataManager.LoadUnitPriceData();
+                    List<CartItem> shoppingCart = DemoDataManager.LoadShoppingCartData();
+                    Dictionary<char, MultibuyPromotion> dictionaryMultibuyPromotion = DemoDataManager.LoadMultibuyPromotions();
+                    Dictionary<char, DuoComboPromotion> dictionaryDuoComboPromotion = DemoDataManager.LoadDuoComboPromotions();
+
+
+                    foreach (CartItem cartItem in shoppingCart)
+                    {
+                        
+
+                    }
+
+                }
+
+                catch (Exception e)
+
+                {
+                        Console.WriteLine(e.Message);
+                }
+
+        }
+
+        public static bool IsMultibuyPromotionApplicable(CartItem cartItem, Dictionary<char, MultibuyPromotion> dictionaryMultibuyPromotion)
+        {
+            if (dictionaryMultibuyPromotion.ContainsKey(cartItem.ProductId))
+            {
+                MultibuyPromotion mp = new MultibuyPromotion();
+                mp = dictionaryMultibuyPromotion[cartItem.ProductId];
+                if (mp.ActiveState && cartItem.Count >= mp.Count)
+
+                    return true;
+
+                else
+
+                    return false;
+            }
+            else
+                return false;
 
         }
     }
